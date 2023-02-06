4f8d9bee480eec3e13a3c75adc4ef689cbf9d152
Joshua Kerr <kerrjoshua@gmail.com>
2023-02-06 15:01:57 +0900

    input field is controlled, value held in local Form state.inputText

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      inputText: ''
+    }
+  }
+
+  handleChange(evt) {
+    this.setState({...this.state, inputText: evt.target.value})
+  }
+
   render() {
-    return <>from form</>
+    return <div>
+      <form>
+        <input type='text' placeholder='New item...' onChange={e => this.handleChange(e)} value={this.state.inputText}></input>
+        <button type='submit'>Add item</button>
+        <div><button>Clear Completed</button></div>
+        
+        
+      </form>
+      </div>
   }
 }
